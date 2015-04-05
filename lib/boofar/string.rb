92b449662733b15d5542ec770ac2beae6af31788
Norbert Melzer <timmelzer@gmail.com>
2015-04-06 00:42:55 +0200

    Documentation and Errorhandling

--- lib/boofar/string.rb
+++ lib/boofar/string.rb
@@ -1,24 +1,67 @@
+class NotAConstant < RuntimeError
+end
+
 class String
+  # Creates a constant from a {String}
+  #
+  # @param s [String] The {String} that shall be converted to a constant
+  # @return A constant equivalent to the given {String} `s`
+  # @raise [ArgumentError] When `s` is not a {String}
   def self.to_constant s
-    s.to_constant
+    if s.is_a? String then
+      s.to_constant
+    else
+      raise ArgumentError.new "String expected, got #{s.class}"
+    end
   end
 
+  # Makes the first character of a {String} upcase
+  #
+  # @param s [String] The {String} that shall get modified
+  # @return [String] `s` with the first character uppercased
+  # @raise [ArgumentError] When `s` is not a {String}
   def self.upcase_first s
-    s.upcase_first
+    if s.is_a? String then
+      s.upcase_first
+    else
+      raise ArgumentError.new "String expected, got #{s.class}"
+    end
   end
 
+  # Makes the first character of a {String} lowercase
+  #
+  # @param s [String] The {String} that shall get modified
+  # @return [String] `s` with the first character lowercased
+  # @raise [ArgumentError] When `s` is not a {String}
   def self.lowercase_first s
-    s.lowercase_first
+    if s.is_a? String then
+      s.lowercase_first
+    else
+      raise ArgumentError.new "String expected, got #{s.class}"
+    end
   end
 
+  # Creates a constant from a {String}
+  #
+  # @return A constant equivalent to the given {String} `s`
   def to_constant
-    self.split('::').inject(Object) { |o,c| o.const_get c }
+    begin
+      self.split('::').inject(Object) { |o,c| o.const_get c }
+    rescue NameError => e
+      raise NotAConstant.new "#{self} is not a valid constant"
+    end
   end
 
+  # Makes the first character of a {String} upcase
+  #
+  # @return [String] `s` with the first character uppercased
   def upcase_first
     self.slice(0,1).capitalize + self.slice(1..-1)
   end
 
+  # Makes the first character of a {String} upcase
+  #
+  # @return [String] `s` with the first character uppercased
   def lowercase_first
     self.slice(0,1).downcase + self.slice(1..-1)
   end
